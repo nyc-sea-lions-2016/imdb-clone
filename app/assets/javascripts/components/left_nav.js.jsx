daf4ed2442c75b4dbd39cc58403721e241d7c1a7
Alyssa <aransbury@gmail.com>
2016-04-18 18:51:54 -0400

    Update color of links on left nav

--- app/assets/javascripts/components/left_nav.js.jsx
+++ app/assets/javascripts/components/left_nav.js.jsx
@@ -8,9 +8,9 @@ var LeftNav = React.createClass({
       <div id="sidebar" role="navigation">
           <div id="sidebar-nav">
             <ul className="nav nav-stacked">
-                <li><a href="#"> Movies</a></li>
-                <li><a href="#"> Categories</a></li>
-                <li><a href="#"> Actors</a></li>
+                <li ><a href="#" className='left-nav-button'> Movies</a></li>
+                <li ><a href="#" className='left-nav-button'> Categories</a></li>
+                <li ><a href="#" className='left-nav-button'> Actors</a></li>
             </ul>
           </div>
       </div>
