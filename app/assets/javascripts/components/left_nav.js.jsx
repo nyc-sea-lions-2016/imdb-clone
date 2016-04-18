3faee0ad118695ff0720be7f14d61dd103846713
John Maguire <jmaguire5588@gmail.com>
2016-04-18 15:26:25 -0400

    Make controller for films and reviews

--- app/assets/javascripts/components/left_nav.js.jsx
+++ app/assets/javascripts/components/left_nav.js.jsx
@@ -2,13 +2,7 @@ var LeftNav = React.createClass({
 
   render: function() {
     return (
-      <nav className='left-nav'>
-        <ul>
-          <li>Movies</li>
-          <li>Categories</li>
-          <li>Actors</li>
-        </ul>
-      </nav>
+
     );
   }
 });
