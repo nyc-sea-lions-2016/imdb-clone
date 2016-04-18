9dbff6ca3aaac83feb6774313cf0b8167169958b
Mike Gwozdek <email": null>
2016-04-18 16:16:07 -0400

    Add controllers and views for sessions and users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       session[:id] = @user.id
+      # TODO determine where to redirect
     else
-      render :login
+      # TODO determine what to render if incorrect login details entered
   end
 
   def logout
     session.clear
+    # TODO determine where to redirect
   end
 end
