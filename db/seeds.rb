3057d8fa1a8da6a388800788c686e1ccfaa94355
Mike Gwozdek <email": null>
2016-04-19 10:38:56 -0400

    Update seed data, add devise user views, display login/logout views

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,11 +29,8 @@ end
 ROLES = ['admin', 'regular', 'trusted']
 20.times do
   User.create!({
-    username: Faker::Internet.user_name,
     email: Faker::Internet.safe_email,
-    password: '123',
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+    password: '123456',
     role_id: (1..3).to_a.sample,
     })
 end
