2eeba199b4d5869351eea4489130e840580e9de0
John Maguire <jmaguire5588@gmail.com>
2016-04-19 08:45:55 -0400

    added average ratings function to ratings model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,6 +5,15 @@ class Rating < ActiveRecord::Base
   validates :value, :user_id, :film_id, presence: true
   validate :rating_value
 
+  def average_ratings
+    @films = Film.all
+    @ratings_hash = Hash.new
+    @films.each do |film|
+      id = film.id
+      @ratings_hash[:id] = film.average_rating
+    end
+  end
+
   private
   def rating_value
     value.between?(0,5) ? true : errors.add(:base, 'You must rate this between 0 and 5 stars')
