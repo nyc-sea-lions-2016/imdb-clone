8705f838413978bbb2c6e733c3fffce46715da5c
John Maguire <jmaguire5588@gmail.com>
2016-04-19 11:05:19 -0400

    comment out handlefilmclick until working

--- app/assets/javascripts/components/film/film_list.js.jsx
+++ app/assets/javascripts/components/film/film_list.js.jsx
@@ -1,12 +1,15 @@
 var FilmList = React.createClass({
-
+  handleClick: function(data){
+    debugger;
+  },
   render: function() {
+    var self = this;
     var filmNodes = this.props.films.map(function(film){
       return (
-        <Film key={film.id} data={film} />
+        <Film key={film.id} data={film} onClick={self.handleClick} />
       )
     });
-    
+
     return (
       <div className='filmList list-group' onClick={this.handleClick}>
         <ul>
