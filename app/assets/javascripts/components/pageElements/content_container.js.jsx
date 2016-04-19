8350752300cc10911e682fbc9c32ba7f52a36106
Alyssa <aransbury@gmail.com>
2016-04-19 11:57:23 -0400

    Add logic so that when content container loads, it sends a call to the server to update films list based on whether someone has clicked a category or not

--- app/assets/javascripts/components/pageElements/content_container.js.jsx
+++ app/assets/javascripts/components/pageElements/content_container.js.jsx
@@ -1,19 +1,34 @@
 var ContentContainer = React.createClass({
   getInitialState: function(){
-    return {films: [], selected: {name: 'all'}}
+    return {films: [], selected: {name: 'All', id: 9}}
+  },
+  componentDidMount: function(){
+    this.showCategory();
   },
   showCategory: function(data){
-    var selected = this.state.categories.filter(function(object){
-      return object.id == data
+    this.state.selected
+    debugger
+    var categoryUrl = '/categories/' + this.state.selected.id
+    //ajax call to get info I want
+    $.ajax({
+      url: categoryUrl,
+      dataType: 'json',
+      method: 'GET',
+      success: function(films){
+        debugger
+        this.setState({films: films});
+      }.bind(this),
+      error: function(xhr,status,err){
+        console.error(this.props.url,status,err.toString())
+      }.bind(this)
     });
-    this.setState({selected: selected[0]})
   },
   render: function() {
     var self = this;
-    var selectedCategory = this.state.selected && this.state.selected.name
+    var selectedCategory = this.state.selected && this.state.selected.id
     return (
       <div id='content-container'>
-        <FilmContainer selectedCategory={selectedCategory}/>
+        <FilmContainer selectedCategory={selectedCategory} films={this.state.films} />
       </div>
     );
   }
