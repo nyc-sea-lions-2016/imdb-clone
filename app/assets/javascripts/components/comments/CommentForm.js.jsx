9c3d1628ee743170336953add1bd0fa6ae8972b2
Alyssa <aransbury@gmail.com>
2016-04-20 11:08:01 -0400

    Add nested reviews / comments route for creating comments

--- app/assets/javascripts/components/comments/CommentForm.js.jsx
+++ app/assets/javascripts/components/comments/CommentForm.js.jsx
@@ -8,9 +8,9 @@ var CommentForm = React.createClass({
   },
   handleSubmit: function(e){
     e.preventDefault();
-    debugger;
-    var commentContent = this.state.content.trim();
-    var url = '/reviews/' + 1 + '/comments'
+    var commentContent = this.state[''].trim();
+    var reviewId = this.props.reviewId
+    var url = '/reviews/' + reviewId + '/comments'
     var content = {comment: {content: commentContent}};
     if (!commentContent){
       return;
@@ -32,7 +32,7 @@ var CommentForm = React.createClass({
     return (
     <div className='form-group'>
       <textarea className="form-control" rows="3" id='comment' placeholder="Leave a comment" onChange={this.handleContentChange}></textarea>
-      <button className='btn btn-default' value='Submit' type='button'>Submit</button>
+      <button onClick={this.handleSubmit} className='btn btn-default' value='Submit' type='button'>Submit</button>
     </div>
     );
   },
