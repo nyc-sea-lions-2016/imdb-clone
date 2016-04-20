3d5575614bc6bffae8e45dcc9912c3cbe616252c
Alyssa <aransbury@gmail.com>
2016-04-19 21:56:42 -0400

    Add up vote and down vote buttons to reviews

--- app/assets/javascripts/components/review/review.js.jsx
+++ app/assets/javascripts/components/review/review.js.jsx
@@ -1,4 +1,12 @@
 var Review = React.createClass({
+  upVote: function(){
+    //send request to server to add a positive vote to this review
+    //remove user's ability to add additional votes to this review
+  },
+  downVote: function(){
+    //send request to server to a negative vote to this review
+    //remove user's ability to add additional votes to this review
+  },
   render: function(){
     var reviewContent = this.props.data.content
     var reviewCreatedAt = this.props.data.created_at
@@ -12,7 +20,7 @@ var Review = React.createClass({
             <h5 className="media-heading">Movie Name</h5>
             <p>{reviewContent}</p>
             <p>Posted on: {reviewCreatedAt} by USERNAME</p>
-            <p>Is this review userful? Tell the reviewer how you feel: <a href='#'>Vote up</a> | <a href='#'>Vote Down</a></p>
+            <p>Is this review userful? Tell the reviewer how you feel: <a href='#' onClick={this.upVote}>Vote up</a> | <a href='#' onClick={this.downVote}>Vote Down</a></p>
           </div>
         </div>
       </li>
