4927ff675ff57c182168edeaf670068a35ba41c5
Mike Gwozdek <email": null>
2016-04-20 08:56:07 -0400

    Add styling changes to films and user forms

--- app/assets/javascripts/components/film/film.js.jsx
+++ app/assets/javascripts/components/film/film.js.jsx
@@ -8,11 +8,16 @@ var Film = React.createClass({
       <li className='list-group-item'>
         <a href='#' onClick={this.handleClick}>
           <section className='film list-group-item'>
+          <div className='media-left'>
+            <a href="#"><img id='review-image' className='media-object' src="http://images.redbox.com/Images/EPC/Kiosk/8003.jpg" alt="movie review image"/></a>
+          </div>
+          <div className="media-body">
             <h4 className='film-name'>{this.props.data.name}</h4><div>Average Rating:{this.props.data.rating}</div>
             <span className='film-year'>Year: {this.props.data.year}, </span>
             <span className='film-director'>Director: {this.props.data.director}, </span>
             <span className='film-oscars'>Oscar Count: {this.props.data.oscar_count}, </span>
             <span className='film-country'>Country: {this.props.data.country} </span>
+          </div>
           </section>
         </a>
       </li>
